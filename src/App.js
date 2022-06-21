9ec90589f01ead7392550ec0ce2f6c5e379ddc38
Le Tan Kiet <kiethohohoho@gmail.com>
2022-06-21 08:50:30 +0700

    react-bpmn with a little custom css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ReactBpmn from "react-bpmn";
+import xml from "./assets/diagram.bpmn";
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 
-function App() {
+function App(props) {
+  function onShown() {
+    console.log("diagram shown");
+  }
+
+  function onLoading() {
+    console.log("diagram loading");
+  }
+
+  function onError(err) {
+    console.log("failed to show diagram");
+  }
+
+  const [diagram, setDiagram] = useState("");
+
+  useEffect(() => {
+    fetch(xml)
+      .then((xml) => xml.text())
+      .then((xml) => {
+        setDiagram(xml);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactBpmn
+      diagramXML={diagram}
+      onShown={onShown}
+      onLoading={onLoading}
+      onError={onError}
+    />
   );
 }
-
 export default App;
